6c07fd56364718dd0e507413001940089efd65b0
JDParay <jdparay@mcm.edu.ph>
2025-11-13 21:05:59 +0800

    CoffeeMeter
    that fucking coffee meter that i hate

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RPSGameController : MonoBehaviour
 {
@@ -24,60 +25,72 @@ public class RPSGameController : MonoBehaviour
     public EnemyReaction scissorsEnemy;
     public SpriteRenderer enemyNeutral;
 
+    [Header("Coffee Meter")]
     public CoffeeMeter coffeeMeter;
 
     void Start()
     {
+        // Subscribe clicks
         rockSprite.OnClicked += () => PlayerChose(RPSChoice.Rock);
         paperSprite.OnClicked += () => PlayerChose(RPSChoice.Paper);
         scissorsSprite.OnClicked += () => PlayerChose(RPSChoice.Scissors);
 
+        // Initialize visuals
         ShowOnly(enemyNeutral);
+
+        // Optional: VN-style pre-game dialogue
+        if (coffeeMeter != null)
+        {
+            coffeeMeter.OnMaxReached += () =>
+            {
+                Debug.Log("Coffee Meter Full! Trigger VN or mini-game.");
+                // Trigger visual novel sequence here
+            };
+            coffeeMeter.OnMinReached += () =>
+            {
+                Debug.Log("Coffee Meter Empty! Trigger VN or mini-game.");
+                // Trigger alternate sequence here
+            };
+        }
     }
 
     void PlayerChose(RPSChoice playerChoice)
-{
-    RPSChoice enemyChoice = (RPSChoice)Random.Range(0, 3);
-    Debug.Log($"You: {playerChoice} | Enemy: {enemyChoice}");
-
-    int result = DetermineWinner(playerChoice, enemyChoice);
-    HideAll();
-
-    EnemyReaction chosen = GetEnemyReaction(enemyChoice);
-
-    if (result == 1)
     {
-        // Player wins -> enemy mad
-        chosen.mad.gameObject.SetActive(true);
-        Debug.Log("You Win!");
-        if (coffeeMeter != null)
-            coffeeMeter.Increase();
-    }
-    else if (result == -1)
-    {
-        // Enemy wins -> enemy happy
-        chosen.happy.gameObject.SetActive(true);
-        Debug.Log("Enemy Wins!");
-        if (coffeeMeter != null)
-            coffeeMeter.Decrease();
-    }
-    else
-    {
-        // Draw -> show draw sprite for 2s, then revert to neutral
-        if (chosen.draw != null)
+        RPSChoice enemyChoice = (RPSChoice)Random.Range(0, 3);
+        Debug.Log($"You: {playerChoice} | Enemy: {enemyChoice}");
+
+        int result = DetermineWinner(playerChoice, enemyChoice);
+        HideAll();
+
+        EnemyReaction chosen = GetEnemyReaction(enemyChoice);
+
+        if (result == 1)
         {
-            StartCoroutine(ShowDrawTemporarily(chosen.draw, 2f));
+            // Player wins -> enemy mad
+            chosen.mad.gameObject.SetActive(true);
+            Debug.Log("You Win!");
+            if (coffeeMeter != null)
+                coffeeMeter.Increase();
         }
-        else if (enemyNeutral != null)
+        else if (result == -1)
         {
-            ShowOnly(enemyNeutral);
+            // Enemy wins -> enemy happy
+            chosen.happy.gameObject.SetActive(true);
+            Debug.Log("Enemy Wins!");
+            if (coffeeMeter != null)
+                coffeeMeter.Decrease();
         }
+        else
+        {
+            // Draw -> show draw sprite for 2s, then revert to neutral
+            if (chosen.draw != null)
+                StartCoroutine(ShowDrawTemporarily(chosen.draw, 1f));
+            else if (enemyNeutral != null)
+                ShowOnly(enemyNeutral);
 
-        // Coffee meter unchanged on draw
-        Debug.Log("Draw!");
+            Debug.Log("Draw!");
+        }
     }
-}
-
 
     int DetermineWinner(RPSChoice player, RPSChoice enemy)
     {
@@ -108,13 +121,15 @@ public class RPSGameController : MonoBehaviour
 
         rockEnemy.happy.gameObject.SetActive(false);
         rockEnemy.mad.gameObject.SetActive(false);
-        rockEnemy.draw.gameObject.SetActive(false);
+        if (rockEnemy.draw != null) rockEnemy.draw.gameObject.SetActive(false);
+
         paperEnemy.happy.gameObject.SetActive(false);
         paperEnemy.mad.gameObject.SetActive(false);
-        paperEnemy.draw.gameObject.SetActive(false);
+        if (paperEnemy.draw != null) paperEnemy.draw.gameObject.SetActive(false);
+
         scissorsEnemy.happy.gameObject.SetActive(false);
         scissorsEnemy.mad.gameObject.SetActive(false);
-        scissorsEnemy.draw.gameObject.SetActive(false);
+        if (scissorsEnemy.draw != null) scissorsEnemy.draw.gameObject.SetActive(false);
     }
 
     void ShowOnly(SpriteRenderer sprite)
@@ -136,12 +151,10 @@ public class RPSGameController : MonoBehaviour
         // Wait for the duration
         yield return new WaitForSeconds(duration);
 
-        // Hide all draw/happy/mad sprites before showing neutral
         HideAll();
 
         // Show neutral
         if (enemyNeutral != null)
             enemyNeutral.gameObject.SetActive(true);
     }
-
 }
